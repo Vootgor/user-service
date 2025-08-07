e0404faec4cf936159ccc630c1a6f93c77ad2d91
Georgii Voronov <ld0532182@gmail.com>
2025-08-07 15:59:44 +0200

    US4 split service to use cases

--- src/main/java/com/bikebuilder/userservice/application/port/usecase/CreateUserUseCaseImpl.java
+++ src/main/java/com/bikebuilder/userservice/application/port/usecase/CreateUserUseCaseImpl.java
@@ -1,34 +1,21 @@
-package com.bikebuilder.userservice.application.port.service;
+package com.bikebuilder.userservice.application.port.usecase;
 
 import com.bikebuilder.userservice.adapter.in.CreateUserUseCase;
-import com.bikebuilder.userservice.adapter.in.GetUserFromIdUseCase;
 import com.bikebuilder.userservice.application.port.in.command.UserCreateCommand;
-import com.bikebuilder.userservice.application.port.out.GetUserFromIdPort;
 import com.bikebuilder.userservice.application.port.out.SaveUserPort;
 import com.bikebuilder.userservice.domain.model.User;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements
-    CreateUserUseCase,
-    GetUserFromIdUseCase
-{
+public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
     private final SaveUserPort saveUserPort;
-    private final GetUserFromIdPort getUserFromIdPort;
 
     @Override
     public User createUser(UserCreateCommand command) {
         User user = User.create(command);
         return saveUserPort.save(user);
-    }
-
-
-    @Override
-    public User getUser(UUID id) {
-        return getUserFromIdPort.getUser(id);
     }
 }
